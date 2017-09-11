70022d740700589757ad86bf7ec5ce963a5a33ce
Alejandro Sanabria <alesanabriav@gmail.com>
2017-09-11 10:10:49 -0500

    add theme

---
+++ theme/types/project.php
@@ -0,0 +1,27 @@
+<?php
+
+function create_project_type() {
+
+  register_post_type( 'proyect',
+    [
+      'labels' => [
+        'name' => 'Projects',
+        'singular_name' => 'Project'
+			],
+			'supports' => [
+          'title',
+          'editor',
+          'excerpt',
+          'thumbnail',
+          'revisions',
+          'page_image_square'
+        ],
+			'taxonomies' => [ 'category'],
+      'public' => true,
+      'has_archive' => true
+		]
+  );
+
+}
+
+add_action( 'init', 'create_project_type' );
