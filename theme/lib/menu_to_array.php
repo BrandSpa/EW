8521cc11bf04802946cbaa38e42a52ce077e0931
Alejandro Sanabria <alesanabriav@gmail.com>
2017-11-12 18:45:56 -0500

    double check

--- theme/lib/menu_to_array.php
+++ theme/lib/menu_to_array.php
@@ -4,7 +4,7 @@ function menu_to_array($locationName) {
 
   if(isset(get_nav_menu_locations()[$locationName])) {
     $menuId = get_nav_menu_locations()[$locationName];
-    return wp_get_nav_menu_items($menuId);
+    return wp_get_nav_menu_items($menuId) ? wp_get_nav_menu_items($menuId) : [];
   } else {
     return array();
   }
