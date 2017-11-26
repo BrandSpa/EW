8592ada6d1fda1983d761eb538d15f75d42604c5
Alejandro Sanabria <alesanabriav@gmail.com>
2017-11-25 23:48:55 -0500

    location data

---
+++ theme/lib/location_data.php
@@ -0,0 +1,78 @@
+<?php
+function locationData() {
+	$countries = [
+		'Aruba',
+		'Colombia',
+		'Panamá',
+		'USA'
+	];
+	
+	$states = [
+		'Florida',
+		'Maryland',
+		'New Jersey',
+		'New York',
+		'Ohio',
+		'Pennsylvania',
+		'Tennessee',
+		'Texas',
+		'Virginia',
+		'Washington',
+		'Antioquia',
+		'Atlántico',
+		'Bolívar',
+		'Cundinamarca',
+		'Magdalena',
+		'Cordoba',
+		'Valle del Cauca'
+	];
+	
+	$cities = [
+		'Washington',
+		'Akron',
+		'Arraijan',
+		'Aruba',
+		'Baltimore',
+		'Bay harbor Islands',
+		'Boyton Beach',
+		'Buenaventura',
+		'Ciudad de panamá',
+		'Cleveland',
+		'Columbus',
+		'Dallas',
+		'Fort lauderdale',
+		'Fort Lee',
+		'Galveston',
+		'Hallandale Beach',
+		'Hollywood',
+		'Houston',
+		'Key Biscane',
+		'McLean',
+		'Miami',
+		'Nashville',
+		'New york',
+		'Orlando',
+		'Panamá',
+		'Philadelphia',
+		'Phoenix',
+		'Sunny Isles Beach',
+		'West Palm Beach',
+		'Medellín',
+		'Barranquilla',
+		'Cartagena',
+		'Bogotá',
+		'Santa Marta',
+		'Montería',
+		'Cali',
+		'Ciudad de Panamá',
+		'Arraijan',
+		'Buenaventura',
+		'Panamá Pacífico'
+	];
+	
+	return [
+		'countries' => $countries,
+		'states' => $states,
+		'cities' => $cities
+	];
+}
