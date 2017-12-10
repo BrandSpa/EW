0356891bdf464409d5adddff5e40ebd575e5efe2
Alejandro Sanabria <alesanabriav@gmail.com>
2017-12-10 13:43:08 -0500

    change layout

--- theme/footer.php
+++ theme/footer.php
@@ -1,30 +1,45 @@
 
 <section class="footer">
-<div class="container">
-  <div class="row">
-    <div class="col-lg-6"></div>
-    <div class="col-lg-6">
-    <h4>CONTACT US</h4>
-    <?php echo do_shortcode('[ew_contact_us]') ?>
-    </div>
-  </div>
-</div>
+
+      <div class="col-xs-1 col-sm-1"></div>
+      <div class="col-lg-5 col-xs-10">
+      <h4>CONTACT US</h4>
+        <?php echo do_shortcode('[ew_contact_us]') ?>
+      </div>
+      <div class="col-lg-5 col-xs-10">
+        <h4>CONTACT US</h4>
+        <?php echo do_shortcode('[ew_contact_us]') ?>
+      </div>
+      <div class="col-xs-1 col-sm-1"></div>
 
 </section>
 
 <style>
-.footer {
-  position: relative;
-  z-index: 100;
-  width: 100%;
-  background: #000;
-  padding-top: 150px;
-  padding-bottom: 50px;
-}
+  .footer {
+    position: relative;
+    z-index: 100;
+    width: 100%;
+    background: #000;
+    padding-top: 150px;
+    padding-bottom: 50px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
 
-.footer h4 {
-  color: #fff;
-}
+  .footer h4 {
+    color: #fff;
+  }
+
+  .footer-bottom {
+    border-top: 1px solid blue;
+  }
+
+  @media (min-width: 1024px) {
+    .footer {
+      flex-direction: row;
+    }
+  }
 </style>
 <script>
   window.templateUri = '<?php echo get_template_directory_uri() ?>';
--- theme/index.php
+++ theme/index.php
@@ -1,8 +1,12 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="container">
-<?php the_content() ?>
+<div class="row">
+  <div class="col-lg-1 col-md-1 col-sm-1"></div>
+  <div class="col-lg-10 col-md-10 col-sm-10">
+    <?php the_content() ?>
+  </div>
+  <div class="col-lg-1 col-md-1 col-sm-1"></div>
 </div>
 <?php endwhile; else : ?>
   <p><?php _e( '401' ); ?></p>
