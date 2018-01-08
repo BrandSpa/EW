706c3e1a4349f615f8383165ffba29802cafaba9
Alejandro Sanabria <alesanabriav@gmail.com>
2018-01-08 15:30:54 -0500

    fix get terms

--- theme/functions.php
+++ theme/functions.php
@@ -16,13 +16,13 @@ register_nav_menus(
 require_once 'config/db.php';
 require_once 'types/index.php';
 require_once 'api/index.php';
-require_once 'lib/index.php';
+
 require_once 'taxonomies/index.php';
+require_once 'lib/index.php';
 require_once 'metaboxes/index.php';
 require_once 'shortcodes/index.php';
 require_once 'models/index.php';
 
-set_translations();
 
 function gett($text) {
 	if(function_exists('pll__') ) {
--- theme/lib/translations.php
+++ theme/lib/translations.php
@@ -13,9 +13,9 @@ function register_translation($name ='', $text, $group = 'EW', $multiline = true
 
 function set_translations() {
 	$products = get_terms('product', [ 'hide_empty' => 0 ]);
-	$brands = get_terms('brands', [ 'hide_empty' => 0 ]);
+	$brands = get_terms('brand', ['hide_empty' => 0 ]);
 	$types = get_terms('type', [ 'hide_empty' => 0 ]);
-	$features = get_terms('features', [ 'hide_empty' => 0 ]);
+	$features = get_terms('feature', [ 'hide_empty' => 0 ]);
 	$months = function_exists('months') ? months() : [];
 
 	$texts = [
@@ -68,3 +68,5 @@ function set_translations() {
 
 
 }
+
+add_action('init', 'set_translations');
