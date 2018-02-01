9eaa49d9b47cc4e6f14f66d535ef8376ac4b394e
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-01 17:56:06 -0500

    arrow load function

--- theme/client/lib/arrow.js
+++ theme/client/lib/arrow.js
@@ -1,13 +1,15 @@
 
 const arrowAction = () =>{
-
-    (function($){
-        $(document).on('click', '.arrow svg', function(){
-            $('html,body').animate({
-            scrollTop: $(".project__content").offset().top-100},
-            'slow');
-        })
-    })(jQuery)
+    window.load(function(){
+        (function($){
+            $(document).on('click', '.arrow svg', function(){
+                $('html,body').animate({
+                scrollTop: $(".project__content").offset().top-100},
+                'slow');
+            })
+        })(jQuery);
+    })
+    
 
 }
 
