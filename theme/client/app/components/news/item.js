dddd62377de9dfaffb014f4967c409fe7a24738f
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-02 11:20:20 -0500

    add news to script

---
+++ theme/client/app/components/news/item.js
@@ -0,0 +1,133 @@
+import React from 'react';
+
+const NewItem = ({ news }) => (
+
+	<a href={news.url.replace('#038;', '&')} className="project-item">
+		<div
+			className="project-item__header"
+			style={{ backgroundImage: `url(${news.thumb})` }}
+		/>
+
+		<div className="project__info">
+			<span className="project__info-date">{news.month} {news.year}</span>
+			<div className="miniline" />
+			<span className="project__info-title">{news.name}</span>
+			<div className="project__info__bottom">
+				<span className="project__info-country">{news.country}</span>
+				<span className="project__info-state-city">{news.state} · {news.city}</span>
+			</div>
+		</div>
+
+		<style jsx>{`
+        .project-item {
+            display: flex;
+            flex: 1;
+            transition: transform .1s ease-in-out;
+            position: relative;
+            background: #fff;
+            width: 100%;
+            margin-bottom: 20px;
+          }
+
+          .miniline {
+            width: 15px;
+            height: 1px;
+            background: #039ED8;
+            margin: 5px 0;
+          }
+
+          .project-item:hover {
+            transform: scale(1.150);
+            z-index: 2;
+            box-shadow: 0 0 20px rgba(0,0,0,.1)
+          }
+
+          .project-item__header {
+            width: 40%;
+            min-height: 140px;
+            background-position: center;
+            background-size: cover;
+            position: relative
+          }
+
+
+          .project__info {
+            padding: 10px 20px;
+            color: #1E9CC0;
+            width: 70%;
+            position: relative;
+          }
+
+          .project__info__bottom {
+            position: absolute;
+            bottom: 10px;
+          }
+
+          .project__info span {
+            display: block;
+          }
+
+          .project__info-title {
+            font-size: 15px;
+            color: #5D5D5D;
+            margin-bottom: 10px;
+          }
+
+          .project__info-date {
+            font-size: 13px;
+          }
+
+          .project__info-country, .project__info-state-city {
+            font-size: 13px;
+            color: #039ED8;
+          }
+
+          @media (min-width: 1024px) {
+            .project-item {
+              display: block;
+              margin-bottom: 2px;
+            }
+
+            .project-item__header {
+              height: 200px;
+              width: 100%;
+            }
+
+            .project__info {
+              height: 200px;
+              padding: 20px;
+              width: 100%;
+            }
+
+            .project__info__bottom {
+              position: absolute;
+              bottom: 20px;
+            }
+
+            .project__info-title {
+              font-size: 17px;
+              margin-bottom: 30px;
+              width: 100%;
+            }
+
+            .miniline {
+              margin: 10px 0;
+            }
+
+            .project__info-date {
+              font-size: 15px;
+            }
+
+            .project__info-country, .project__info-state-city {
+              font-size: 15px;
+            }
+
+          }
+
+
+        `}
+		</style>
+	</a>
+);
+
+export default NewItem;
---
+++ theme/client/app/components/news/section.js
@@ -0,0 +1,325 @@
+import React, { Component } from 'react';
+import { createApolloFetch } from 'apollo-fetch';
+import { findIndex, throttle } from 'lodash';
+import classnames from 'classnames';
+import New from './item';
+
+const uri = '/wp-content/themes/theme/graphql/index.php';
+const apolloFetch = createApolloFetch({ uri });
+
+const projectsQuery = `
+query($metaQuery: [metaQuery], $taxQuery: [taxonomyQuery], $paged: Int){
+  projects(posts_per_page: 12, paged: $paged, meta_query: $metaQuery, tax_query: $taxQuery) {
+		id
+    thumb
+		name
+		url
+    country
+    state
+		city
+		year
+		month
+  }
+}
+`;
+
+class ProjectsSection extends Component {
+  state = {
+  	projects: [],
+  	metaQuery: [],
+  	taxQuery: [],
+  	showFilters: false,
+  	inBound: false,
+  	inTop: false,
+  	inBottom: false,
+  	paged: 1,
+  }
+
+  componentDidMount = () => {
+  	this.getProjects();
+  	if (window.matchMedia('(max-width: 1024px)').matches) {
+  		window.addEventListener('scroll', throttle(this.stickFilters, 150));
+  	}
+  }
+
+  componentWillUnmount() {
+  	if (window.matchMedia('(max-width: 1024px)').matches) {
+  		window.removeEventListener('scroll', throttle(this.stickFilters, 150));
+  	}
+  }
+
+  getProjects = async (variables = {}) => {
+  	try {
+  		const res = await apolloFetch({ query: projectsQuery, variables });
+  		this.setState({ projects: [...this.state.projects, ...res.data.projects] });
+  	} catch (err) {
+  		console.log('get projects err: ', err);
+  	}
+  }
+
+	stickFilters = () => {
+		const { container, filters } = this;
+		const bound = container.getBoundingClientRect();
+		const containerHeight = container.clientHeight;
+		const h = filters.clientHeight;
+
+		if (bound.top < 0, bound.bottom > 0) {
+			this.setState({ inBound: true });
+		}
+
+		if (!(bound.top < 0 && (bound.bottom - h) - 40 > 0) && bound.y < 0) {
+			this.setState({ inBound: false });
+		}
+
+		if (!(bound.top < 0 && (bound.bottom - h) - 40 > 0) && bound.y > 0) {
+			this.setState({ inBound: false });
+		}
+	}
+
+	updateOrAdd = (filter, metaQuery) => {
+		let q = [];
+
+		if (findIndex(metaQuery, { key: filter.key }) !== -1) {
+			q = metaQuery.map((query) => {
+				if (query.key !== filter.key) return query;
+				return { ...query, ...filter };
+			});
+		} else {
+			q = [...metaQuery, filter];
+		}
+
+		return q;
+	}
+
+	removeFilter = (key, metaQuery) => {
+		const	q = metaQuery.filter(query => query.key !== key);
+		return q;
+	}
+
+  handleFilters = (filters) => {
+  	let { metaQuery } = this.state;
+
+  	if (filters.country.length > 0) {
+  		const country = { key: 'country_key', value: filters.country };
+  		metaQuery = this.updateOrAdd(country, metaQuery);
+  	} else {
+  		metaQuery = this.removeFilter('country_key', metaQuery);
+  	}
+
+  	if (filters.city.length > 0) {
+  		const city = { key: 'city_key', value: filters.city };
+  		metaQuery = this.updateOrAdd(city, metaQuery);
+  	} else {
+  		metaQuery = this.removeFilter('city_key', metaQuery);
+  	}
+
+  	if (filters.state.length > 0) {
+  		const state = { key: 'state_key', value: filters.state };
+  		metaQuery = this.updateOrAdd(state, metaQuery);
+  	} else {
+  		metaQuery = this.removeFilter('state_key', metaQuery);
+  	}
+
+  	this.setState({ projects: [], paged: 1, metaQuery }, () => {
+  		this.getProjects({ metaQuery, taxQuery: this.state.taxQuery });
+  	});
+  }
+
+  handleFiltersProducts = (products) => {
+  	let taxQuery = [];
+
+  	if (products.length > 0) {
+  		const tax = { taxonomy: 'product', terms: products };
+  		taxQuery = [tax];
+  	} else {
+  		taxQuery = [];
+  	}
+
+  	this.setState({ projects: [], taxQuery }, () => {
+  		this.getProjects({ taxQuery, metaQuery: this.state.metaQuery });
+  	});
+  }
+
+	toggleFilters = (e) => {
+		e.preventDefault();
+		this.setState({ showFilters: !this.state.showFilters });
+	}
+
+	handleUrl = (query) => {
+		const { pathname, search } = window.location;
+		const oldSearch = qs.parse(search.replace('?', ''));
+		const newSearch = { ...oldSearch, ...query };
+		const url = `${pathname}?${qs.stringify(newSearch)}`;
+		return url;
+	}
+
+	paginate = (e) => {
+		if (e) e.preventDefault();
+		let { taxQuery, metaQuery, paged } = this.state;
+		paged += 1;
+
+		this.setState({ paged }, () => {
+			this.getProjects({ metaQuery, taxQuery, paged });
+  	});
+	}
+
+	render() {
+		const { projects, showFilters, inBound } = this.state;
+		const { texts } = this.props;
+
+		const filtersStyle = classnames('filters', {
+			'filters--in-bound': inBound,
+		});
+
+  	const filterContainerStyle = classnames('filters-container', {
+  		'filters-container--open': showFilters,
+		});
+
+		const filterToggleStyle = classnames('filters__toggle', {
+			'filters__toggle--open': showFilters,
+		});
+
+  	return (
+  		<section ref={ref => this.container = ref}>
+  			<div className="col-lg-9">
+                {/* <Loading /> */}
+                <div className="projects">
+                    {news.length > 0 ? news.map(item => (
+                        <div key={item.id} className="col-lg-4 col-md-6 project-item">
+                            <New new={item} />
+                        </div>
+                        ))
+                            :
+                            <div className="empty-value">
+                                <h4>{texts.emptyResult}</h4>
+                            </div>
+                        }
+            </div>
+                {news.length > 0 &&
+                    <a href="#" onClick={this.paginate} className="pagination-btn">
+                        <span>{texts.seeMore}</span>
+                        <Points style={{ float: 'right' }} />
+                    </a>
+                }
+            </div>
+
+				<style jsx>{`
+          .projects {
+						padding: 0;
+						margin: 20px 0;
+            display: flex;
+            flex-wrap: wrap;
+            flex-direction: column;
+					}
+
+					.pagination-btn {
+						float: right;
+						margin: 0 40px 20px 0;
+						color: #039ED8;
+						font-size: 20px;
+					}
+
+					.pagination-btn span {
+						display: block;
+					}
+
+
+          .project-item {
+						padding: 5px;
+					}
+
+					.empty-value {
+						height: 100px;
+						display: flex;
+						flex: 1;
+						align-items: center;
+					}
+
+					.empty-value h4 {
+						font-size: 17px;
+						color: #039ED8;
+					}
+
+					.filters-title {
+						font-size: 15px;
+						color: #039ED8;
+					}
+
+					.filters {
+						width: 90%;
+						background: #fff;
+						box-shadow: 0 10px 10px rgba(0,0,0,.1);
+						display: flex;
+						flex-direction: column;
+						padding-top: 20px;
+						margin-left: 15px;
+						transition: .3s ease;
+					}
+
+					.filters--in-bound {
+						position: fixed;
+						left: 0;
+						right: 0;
+						top: 60px;
+						z-index: 100;
+					}
+
+					.filters__toggle {
+						display: flex;
+						align-self: flex-end;
+						cursor: pointer;
+						border: none;
+						background: transparent;
+						font-size: 15px;
+						color: #039ED8;
+						margin-bottom: 20px;
+						align-items: center;
+						outline: none;
+					}
+
+					.filters__toggle i {
+						padding: 0 20px;
+					}
+
+					.filters__toggle--open i::before {
+						transform: rotate(45deg);
+					}
+
+					.filters-container {
+						display: none;
+					}
+
+					.filters-container--open {
+						display: block;
+						padding-bottom: 20px;
+					}
+
+					@media (min-width: 1024px) {
+						.projects {
+							flex-direction: row;
+						}
+
+						.filters {
+							display: initial;
+							box-shadow: none;
+							background: transparent;
+							padding: 0;
+							margin: 0;
+						}
+
+						.filters-container {
+							display: block;
+						}
+
+						.filters__toggle {
+							display: none;
+						}
+					}
+        `}
+				</style>
+  		</section>
+  	);
+	}
+}
+
+export default ProjectsSection;
