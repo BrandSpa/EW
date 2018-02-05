5c178f34fe9e1582adbe46f6ef6e2e53cf54e482
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-05 12:32:12 -0500

    image style

--- theme/single.php
+++ theme/single.php
@@ -105,10 +105,12 @@
 		line-height: 30px;
 	}
 	.image-container{
-		position: relative;
-		top: -50%;
+		position: absolute;
+		margin-top: -50%;
+		transform: translateY(25%);
 		background-color: #fff;
 		box-shadow: 0px 0px 10px 0px rgba(0,0,0,.7);
+		z-index: 2;
 	}
 	.post__content {
 		font-family: 'Arimo', sans-serif;
