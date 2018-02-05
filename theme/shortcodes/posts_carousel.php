c79bb2ecea2f4a500f1cf78ce4dc16456e2ea9e3
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-05 17:40:57 -0500

    escape attr

--- theme/shortcodes/posts_carousel.php
+++ theme/shortcodes/posts_carousel.php
@@ -19,7 +19,7 @@ function posts_carousel_sc( $atts ){
 		$dateYear = date_format($date ,"Y");
 		$post->date =  $dateDay .' '. gett($dateMonth) . ', '. $dateYear;
 		$post->image = get_the_post_thumbnail_url($post->ID);
-		$post->intro = esc_textarea(get_post_meta($post->ID, 'intro_key', true));
+		$post->intro = esc_attr(get_post_meta($post->ID, 'intro_key', true));
 		return $post;
 	}, $query->get_posts());
 
