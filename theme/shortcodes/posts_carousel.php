8b2215489422c47a9228868d2425b56e08b247aa
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-05 17:44:52 -0500

    remove content

--- theme/shortcodes/posts_carousel.php
+++ theme/shortcodes/posts_carousel.php
@@ -20,6 +20,7 @@ function posts_carousel_sc( $atts ){
 		$post->date =  $dateDay .' '. gett($dateMonth) . ', '. $dateYear;
 		$post->image = get_the_post_thumbnail_url($post->ID);
 		$post->intro = esc_attr(get_post_meta($post->ID, 'intro_key', true));
+		$post->content = "";
 		return $post;
 	}, $query->get_posts());
 
