ba0c87e5d58193a53a14966aa1e2d15d383d539f
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-08 11:34:20 -0500

    Animate scrolls to ids

--- theme/client/lib/arrow.js
+++ theme/client/lib/arrow.js
@@ -7,6 +7,15 @@ const arrowAction = () =>{
                 scrollTop: $(".project__content").offset().top-100},
                 'slow');
             })
+
+            $(document).on('click', 'a[href^="#"]', function (event) {
+                event.preventDefault();
+            
+                $('html, body').animate({
+                    scrollTop: $($.attr(this, 'href')).offset().top
+                }, 500);
+            });
+
         })(jQuery);
     }
     
