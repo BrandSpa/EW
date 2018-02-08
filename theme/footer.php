8fee15229b204cf9b14207c484f80dbf5e49532c
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-08 12:10:34 -0500

    add changes to footer

--- theme/footer.php
+++ theme/footer.php
@@ -3,7 +3,17 @@ $current_url = str_replace('//', 'https://', esc_url($_SERVER['HTTP_HOST'].$_SER
 ?>
 
 <div id="backtotop">
-  <img src="<?php echo get_template_directory_uri() ?>/public/img/up.svg" alt="back to top" >
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+
+    width="50px" height="50px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+
+    <path id="arrow-28-icon" fill="#29e" transform="rotate(-90 256,256)" d="M90,256c0,91.755,74.258,166,166,166c91.755,0,166-74.259,166-166c0-91.755-74.258-166-166-166
+
+    C164.245,90,90,164.259,90,256z M462,256c0,113.771-92.229,206-206,206S50,369.771,50,256S142.229,50,256,50S462,142.229,462,256z
+
+    M199.955,168.598l32.263-32.107L352.154,257L232.218,377.51l-32.263-32.107L287.937,257L199.955,168.598z"/>
+
+    </svg>
 </div>
 
 
@@ -155,6 +165,8 @@ $current_url = str_replace('//', 'https://', esc_url($_SERVER['HTTP_HOST'].$_SER
     float: left;
     width: 100%;
     background: #1f1f1f;
+    position: relative;
+    z-index: 2;
   }
 
   .social-icon {
@@ -181,6 +193,8 @@ $current_url = str_replace('//', 'https://', esc_url($_SERVER['HTTP_HOST'].$_SER
       /* text-align: right; */
       flex-direction: row-reverse;
       padding:15px;
+      position: relative;
+      z-index: 2;
     }
 
     .branding-logo img{
