4b1d7feea9df3116ea45babf5d9fb47519ad9116
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-08 12:23:41 -0500

    change back to top arrow

--- theme/footer.php
+++ theme/footer.php
@@ -3,11 +3,13 @@ $current_url = str_replace('//', 'https://', esc_url($_SERVER['HTTP_HOST'].$_SER
 ?>
 
 <div id="backtotop">
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-    width="50px" height="50px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
-      <path stroke-width="1px" id="arrow-28-icon" fill="#29e" transform="rotate(-90 256,256)" d="M90,256c0,91.755,74.258,166,166,166c91.755,0,166-74.259,166-166c0-91.755-74.258-166-166-166
-      C164.245,90,90,164.259,90,256z M462,256c0,113.771-92.229,206-206,206S50,369.771,50,256S142.229,50,256,50S462,142.229,462,256z
-      M199.955,168.598l32.263-32.107L352.154,257L232.218,377.51l-32.263-32.107L287.937,257L199.955,168.598z"/>
+    <svg width="50px" height="50px" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Group" stroke="#039ED8" stroke-width="2">
+                <polyline id="Path" points="10 31.1794085 24.9035512 17.1794085 40 31.1794085"></polyline>
+                <rect id="Rectangle" x="1" y="1" width="48" height="48"></rect>
+            </g>
+        </g>
     </svg>
 </div>
 
