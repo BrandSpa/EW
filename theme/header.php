eaa4821a655c14e3c135c467564c374600face1c
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-28 16:59:51 -0500

    remove logo

--- theme/header.php
+++ theme/header.php
@@ -102,8 +102,7 @@
 <?php
   $props = ["menu" => menu_to_array('header'), 
             "menu_right" => menu_to_array('footer'),
-            "menu_left" => menu_to_array('footer_left'),
-            "logo" => theme_mod_image_logo()
+            "menu_left" => menu_to_array('footer_left')
            ];
 ?>
 
