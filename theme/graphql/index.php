855d6d4368b62c133da02f963d75635249ccdeaf
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-03-01 13:35:18 -0500

    add lang

--- theme/graphql/index.php
+++ theme/graphql/index.php
@@ -15,7 +15,14 @@ if ( ! defined('ABSPATH') ) {
 $postcontent = file_get_contents('php://input');
 $postcontent = str_replace('\n', '', $postcontent);
 $postcontent = json_decode($postcontent, true);
-$lang = isset($postcontent['lang'])? $postcontent['lang']:'en';
+
+if(function_exists( pll_current_language() )){
+	$lang = pll_current_language();
+}else{
+	$lang = isset($postcontent['lang'])? $postcontent['lang']:'en';
+}
+
+
 
 
 require str_replace('graphql' , '', __DIR__) . '/vendor/autoload.php';
