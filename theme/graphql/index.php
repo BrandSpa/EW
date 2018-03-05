84653932a5ea0189752b25139682205fb7730a2e
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-03-05 09:33:20 -0500

    debug

--- theme/graphql/index.php
+++ theme/graphql/index.php
@@ -15,7 +15,7 @@ if ( ! defined('ABSPATH') ) {
 $postcontent = file_get_contents('php://input');
 $postcontent = str_replace('\n', '', $postcontent);
 $postcontent = json_decode($postcontent, true);
-
+print_r(pll_current_language());exit();
 if(function_exists( pll_current_language() )){
 	$lang = pll_current_language();
 }else{
