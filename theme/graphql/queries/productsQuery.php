b7e509d6e951f8fb86beb51d757d8166e5beb53a
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-03-05 10:28:14 -0500

    remove log

--- theme/graphql/queries/productsQuery.php
+++ theme/graphql/queries/productsQuery.php
@@ -43,7 +43,7 @@ $productsQuery = [
 			$args['tax_query'] = $tax_query;
 		}
 		
-		error_log(print_r($args));
+		//error_log(print_r($args));
 
 		$query = new WP_Query($args);
 		$posts = $query->get_posts();
