33420a7857f29481507035c9b8077d50cfb8cd42
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-03-05 10:33:45 -0500

    show query

--- theme/graphql/queries/productsQuery.php
+++ theme/graphql/queries/productsQuery.php
@@ -46,6 +46,7 @@ $productsQuery = [
 		error_log(print_r($args));
 
 		$query = new WP_Query($args);
+		error_log(print_r($query));
 		$posts = $query->get_posts();
 		error_log(print_r($posts));
 		return $posts ;
