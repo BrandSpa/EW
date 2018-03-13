40e679858ca079fcb5522e21364d8a775ce31e6d
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-03-13 14:58:02 -0500

    changes in modal

--- theme/templates/terms_and_conditios.php
+++ theme/templates/terms_and_conditios.php
@@ -1,4 +1,4 @@
-<div class="terms_and_conditions">
+<div class="terms_and_conditions modal-dialog">
     
     <div class="modal-content">
         <div class="modal-header">
@@ -14,37 +14,5 @@
 </div>
 
 <style>
-    .terms_and_conditions{
-        position: fixed;
-        width: 100%;
-        height: 100%;
-        background: rgba(0,0,0,.3);
-        top: 0;
-        left: 0;
-        z-index: 999;
-        display: none;
-    }
-
-    .terms_and_conditions .tmodal{
-        width: 60%;
-        left: 0;
-        right: 0;
-        margin: 0 auto;
-        top: 10%;
-        /* transform: translateY(-50%); */
-        position: absolute;
-        background: #fff;
-        padding: 30px;
-        border-radius: .3em;
-        box-shadow: 0px 0px 2px 1px rgba(0,0,0,.2);
-    }
-    .terms_and_conditions .tmodal .tmodal_footer{
-        padding: 15px 0px;
-        text-align: center;
-    }
-
-    .terms_and_conditions .tmodal .tmodal_footer .accept{
-        height: 4rem;
-        border-radius: .3rem;
-    }
+   
 </style>
