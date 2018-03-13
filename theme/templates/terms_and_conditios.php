ae4efd0b8cdfc55d8d7b481cccd0148ab705ec9c
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-03-13 15:13:52 -0500

    change button

--- theme/templates/terms_and_conditios.php
+++ theme/templates/terms_and_conditios.php
@@ -3,7 +3,7 @@
         
         <div class="modal-content">
             <div class="modal-header">
-                <span class="close">X</span>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
 
             <div class="modal-body">
