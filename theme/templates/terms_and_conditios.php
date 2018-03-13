eda83b3b378e26192f83fe028ce54c52361a71fd
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-03-13 15:11:46 -0500

    add modal body

--- theme/templates/terms_and_conditios.php
+++ theme/templates/terms_and_conditios.php
@@ -5,7 +5,10 @@
             <div class="modal-header">
                 <span class="close">X</span>
             </div>
+
+            <div class="modal-body">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, architecto distinctio. Nobis eius debitis explicabo unde excepturi voluptatum, eaque, porro dolores obcaecati sunt sit adipisci beatae delectus itaque? Officiis, culpa.
+            </div>
 
             <div class="modal-footer">
                 <button class="btn btn-default"><?php echo gett('Aceptar y Descargar') ?></button>
