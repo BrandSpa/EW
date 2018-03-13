f1f087819c15aca233ea01c26ba3225032810e04
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-03-13 10:24:41 -0500

    add modal styles

--- theme/templates/terms_and_conditios.php
+++ theme/templates/terms_and_conditios.php
@@ -5,3 +5,28 @@
     </div>
 
 </div>
+
+<style>
+    .term_and_conditions{
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        background: rgba(255,255,255,.8);
+        top: 0;
+        left: 0;
+        z-index: 999;
+    }
+
+    .term_and_conditions .tmodal{
+        width: 60%;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        top: 10%;
+        /* transform: translateY(-50%); */
+        position: absolute;
+        background: #fff;
+        padding: 30px;
+        border-radius: .3em;
+    }
+</style>
